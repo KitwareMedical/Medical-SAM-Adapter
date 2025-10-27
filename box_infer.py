adf4d9e35634219e2d427a183555eba1e09b1752
Dženan Zukić <dzenan.zukic@kitware.com>
2025-10-27 15:00:42 -0400

    Add validation_sam from function.py

--- box_infer.py
+++ box_infer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+from segment_anything import sam_model_registry
+from einops import rearrange
+
 import function
+from conf import settings
 # from dataset import *
 # from models.discriminatorlayer import discriminator
 from dataset import *
@@ -20,16 +24,9 @@ def main():
 
     GPUdevice = torch.device('cuda', args.gpu_device)
 
-    net = get_network(args, args.net, use_gpu=args.gpu, gpu_device=GPUdevice, distribution=args.distributed)
-
-
     assert os.path.exists(args.sam_ckpt)
-    loc = 'cuda:{}'.format(args.gpu_device)
-    checkpoint = torch.load(args.sam_ckpt, map_location=loc)
+    net = sam_model_registry["vit_b"](checkpoint=args.sam_ckpt).to(device)
     start_epoch = 0
-
-    net.load_state_dict(checkpoint, strict=False)
-
 
     args.path_helper = set_log_dir('logs', args.exp_name)
     logger = create_logger(args.path_helper['log_path'])
@@ -38,14 +35,180 @@ def main():
     '''segmentation data'''
     nice_train_loader, nice_test_loader = get_dataloader(args)
 
-    '''begain valuation'''
-    best_acc = 0.0
-    best_tol = 1e4
-
-    net.eval()
-
     tol, (eiou, edice) = function.validation_sam(args, nice_test_loader, start_epoch, net)
     logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {start_epoch}.')
+
+
+GPUdevice = torch.device('cuda', args.gpu_device)
+pos_weight = torch.ones([1]).cuda(device=GPUdevice) * 2
+criterion_G = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+seed = torch.randint(1, 11, (args.b, 7))
+
+torch.backends.cudnn.benchmark = True
+loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
+scaler = torch.cuda.amp.GradScaler()
+max_iterations = settings.EPOCH
+post_label = AsDiscrete(to_onehot=14)
+post_pred = AsDiscrete(argmax=True, to_onehot=14)
+dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False)
+dice_val_best = 0.0
+global_step_best = 0
+epoch_loss_values = []
+metric_values = []
+
+
+def validation_sam(args, val_loader, epoch, net: nn.Module, clean_dir=True):
+    # eval mode
+    net.eval()
+
+    mask_type = torch.float32
+    n_val = len(val_loader)  # the number of batch
+    ave_res, mix_res = (0, 0, 0, 0), (0,) * args.multimask_output * 2
+    rater_res = [(0, 0, 0, 0) for _ in range(6)]
+    tot = 0
+    hard = 0
+    threshold = (0.1, 0.3, 0.5, 0.7, 0.9)
+    GPUdevice = torch.device('cuda:' + str(args.gpu_device))
+    device = GPUdevice
+
+    if args.thd:
+        lossfunc = DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
+    else:
+        lossfunc = criterion_G
+
+    with tqdm(total=n_val, desc='Validation round', unit='batch', leave=False) as pbar:
+        for ind, pack in enumerate(val_loader):
+            imgsw = pack['image'].to(dtype=torch.float32, device=GPUdevice)
+            masksw = pack['label'].to(dtype=torch.float32, device=GPUdevice)
+            # for k,v in pack['image_meta_dict'].items():
+            #     print(k)
+            if 'pt' not in pack or args.thd:
+                imgsw, ptw, masksw = generate_click_prompt(imgsw, masksw)
+            else:
+                ptw = pack['pt']
+                point_labels = pack['p_label']
+            name = pack['image_meta_dict']['filename_or_obj']
+
+            buoy = 0
+            if args.evl_chunk:
+                evl_ch = int(args.evl_chunk)
+            else:
+                evl_ch = int(imgsw.size(-1))
+
+            while (buoy + evl_ch) <= imgsw.size(-1):
+                if args.thd:
+                    pt = ptw[:, :, buoy: buoy + evl_ch]
+                else:
+                    pt = ptw
+
+                imgs = imgsw[..., buoy:buoy + evl_ch]
+                masks = masksw[..., buoy:buoy + evl_ch]
+                buoy += evl_ch
+
+                if args.thd:
+                    pt = rearrange(pt, 'b n d -> (b d) n')
+                    imgs = rearrange(imgs, 'b c h w d -> (b d) c h w ')
+                    masks = rearrange(masks, 'b c h w d -> (b d) c h w ')
+                    imgs = imgs.repeat(1, 3, 1, 1)
+                    point_labels = torch.ones(imgs.size(0))
+
+                    imgs = torchvision.transforms.Resize((args.image_size, args.image_size))(imgs)
+                    masks = torchvision.transforms.Resize((args.out_size, args.out_size))(masks)
+
+                showp = pt
+
+                mask_type = torch.float32
+                ind += 1
+                b_size, c, w, h = imgs.size()
+                longsize = w if w >= h else h
+
+                if point_labels.clone().flatten()[0] != -1:
+                    # point_coords = samtrans.ResizeLongestSide(longsize).apply_coords(pt, (h, w))
+                    point_coords = pt
+                    coords_torch = torch.as_tensor(point_coords, dtype=torch.float, device=GPUdevice)
+                    labels_torch = torch.as_tensor(point_labels, dtype=torch.int, device=GPUdevice)
+                    if (len(point_labels.shape) == 1):  # only one point prompt
+                        coords_torch, labels_torch, showp = coords_torch[None, :, :], labels_torch[None, :], showp[
+                            None, :, :]
+                    pt = (coords_torch, labels_torch)
+
+                '''init'''
+                if hard:
+                    true_mask_ave = (true_mask_ave > 0.5).float()
+                    # true_mask_ave = cons_tensor(true_mask_ave)
+                imgs = imgs.to(dtype=mask_type, device=GPUdevice)
+
+                '''test'''
+                with torch.no_grad():
+                    imge = net.image_encoder(imgs)
+                    if args.net == 'sam' or args.net == 'mobile_sam':
+                        se, de = net.prompt_encoder(
+                            points=pt,
+                            boxes=None,
+                            masks=None,
+                        )
+                    elif args.net == "efficient_sam":
+                        coords_torch, labels_torch = transform_prompt(coords_torch, labels_torch, h, w)
+                        se = net.prompt_encoder(
+                            coords=coords_torch,
+                            labels=labels_torch,
+                        )
+
+                    if args.net == 'sam':
+                        pred, _ = net.mask_decoder(
+                            image_embeddings=imge,
+                            image_pe=net.prompt_encoder.get_dense_pe(),
+                            sparse_prompt_embeddings=se,
+                            dense_prompt_embeddings=de,
+                            multimask_output=(args.multimask_output > 1),
+                        )
+                    elif args.net == 'mobile_sam':
+                        pred, _ = net.mask_decoder(
+                            image_embeddings=imge,
+                            image_pe=net.prompt_encoder.get_dense_pe(),
+                            sparse_prompt_embeddings=se,
+                            dense_prompt_embeddings=de,
+                            multimask_output=False,
+                        )
+                    elif args.net == "efficient_sam":
+                        se = se.view(
+                            se.shape[0],
+                            1,
+                            se.shape[1],
+                            se.shape[2],
+                        )
+                        pred, _ = net.mask_decoder(
+                            image_embeddings=imge,
+                            image_pe=net.prompt_encoder.get_dense_pe(),
+                            sparse_prompt_embeddings=se,
+                            multimask_output=False,
+                        )
+
+                    # Resize to the ordered output size
+                    pred = F.interpolate(pred, size=(args.out_size, args.out_size))
+                    tot += lossfunc(pred, masks)
+
+                    '''vis images'''
+                    if ind % args.vis == 0:
+                        namecat = 'Test'
+                        for na in name[:2
+
+                        ]:
+                            img_name = na.split('/')[-1].split('.')[0]
+                            namecat = namecat + img_name + '+'
+                        vis_image(imgs, pred, masks, os.path.join(args.path_helper['sample_path'],
+                                                                  namecat + 'epoch+' + str(epoch) + '.jpg'),
+                                  reverse=False, points=showp)
+
+                    temp = eval_seg(pred, masks, threshold)
+                    mix_res = tuple([sum(a) for a in zip(mix_res, temp)])
+
+            pbar.update()
+
+    if args.evl_chunk:
+        n_val = n_val * (imgsw.size(-1) // evl_ch)
+
+    return tot / n_val, tuple([a / n_val for a in mix_res])
 
 
 if __name__ == '__main__':
